ab9b06cfe9e973527a88ffc23c710aebfffe1f59
Emily Sombra <emmevsombra@gmail.com>
2024-08-26 22:05:14 -0300

    fix requests

--- src/controller/request.ts
+++ src/controller/request.ts
@@ -2,11 +2,16 @@ import axios from "axios";
 // models
 import { Item } from "../models/grid";
 
+const API = axios.create({
+    baseURL: 'http://127.0.0.1:8000'
+})
+
 export const getList = async (url: string): Promise<Item[]> => {
     var finalResponse: Item[] = [];
 
-    await axios.get(url)
+    await API.get(url)
         .then((response) => {
+            console.log(response.data)
             if (Array.isArray(response.data))
                 finalResponse = response.data;
           })
