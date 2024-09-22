e2e383ebc7f849c4d738515f495fe1ab667afe54
Emily Sombra <emmevsombra@gmail.com>
2024-09-22 17:48:20 -0300

    update style

--- src/view/components/blog_item/index.tsx
+++ src/view/components/blog_item/index.tsx
@@ -3,11 +3,13 @@ import { useParams } from "react-router-dom";
 import { Item } from "../../../models/grid";
 import { getCareerItem, getCourseItem } from "../../../controller/http_client";
 import { BlogItemParams, BlogItemProps } from "../../../models/blog";
+import { BlogPostContainer } from "./style";
+import { AppContainer } from "../../../styles/global";
 
 export default function BlogItem({type='careers'}: BlogItemProps){
     const {id = ""} = useParams<BlogItemParams>()
     // state
-    const [item, setItem] = useState<Item>();
+    const [item, setItem] = useState<Item>({});
 
     // hooks
     useEffect(() => {
@@ -18,6 +20,11 @@ export default function BlogItem({type='careers'}: BlogItemProps){
     }, []);
 
     return (
-        <h1>{item?.title}</h1>
+        <AppContainer>
+            <BlogPostContainer>
+                <h1>{item.title}</h1>
+            </BlogPostContainer> 
+        </AppContainer>
+            
     )
 }
---
+++ src/view/components/blog_item/style.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const BlogPostContainer = styled.section`
+    
+`
