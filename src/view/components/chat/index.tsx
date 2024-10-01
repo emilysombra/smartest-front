c354c2e03efcb7187ccbc536fec5e21ecc61d2f4
Emily Sombra <emmevsombra@gmail.com>
2024-09-30 23:30:28 -0300

    styling chatbot

--- src/view/components/chat/index.tsx
+++ src/view/components/chat/index.tsx
@@ -1,15 +1,28 @@
 import { useState } from "react";
-import { ChatContainer } from "./style";
+import { ChatContainer, ChatItem, WelcomeBoxChat } from "./style";
 
 export default function Chat(){
     const [inputPrompt, setInputPrompt] = useState("")
-    const [chatLog, setChatLog] = useState([])
+    const [chatLog, setChatLog] = useState(["", ""])
+
+    const onClickSubmit = async () => {
+        alert("deu certo")
+    }
 
     return (
         <ChatContainer>
             <h1>
                 Teste Chat
             </h1>
+            {chatLog.length > 0 ? (
+                <ChatItem>
+                    teste
+                </ChatItem>
+            ) : (
+                <WelcomeBoxChat>
+                    Boas vindas ao Chatbot do Smartest!!
+                </WelcomeBoxChat>
+            )}
         </ChatContainer>
     )
 }
--- src/view/components/chat/style.ts
+++ src/view/components/chat/style.ts
@@ -1,3 +1,18 @@
 import styled from "styled-components";
 
-export const ChatContainer = styled.section``
+export const ChatContainer = styled.section`
+    flex: 1;
+    background-color: #fafafa;
+    position: relative;
+    line-height: 24px;
+    color: #61656b;
+    font-size: 16px;
+    height: 80vh;
+    overflow-y: auto;
+`
+
+export const WelcomeBoxChat = styled.div``
+
+export const ChatItem = styled.div`
+    
+`
