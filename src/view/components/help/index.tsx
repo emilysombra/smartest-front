d0f47c3ec9fc3c0db17335e5ac43cb0857e3d945
Emily Sombra <emmevsombra@gmail.com>
2025-01-17 23:33:11 -0300

    improving help content

--- src/view/components/help/index.tsx
+++ src/view/components/help/index.tsx
@@ -1,15 +1,28 @@
 import { AppContainer } from "../../../styles/global";
-import { Content, Title } from "./style";
+import { Container, Content, Title } from "./style";
 
 export default function Help(){
     return (
         <AppContainer>
-            <section>
+            <Container>
                 <Title>Como utilizar o Smartest:</Title>
                 <Content>
                     <h2>Olá, boas vindas ao Smartest!</h2>
+                    <p>O Smartest é uma plataforma de ensino de teste de software desenvolvido por estudantes de Ciência da Computação
+                        da Universidade Estadual do Ceará. Na página inicial, você encontrará conteúdos explicativos sobre testes, bem
+                        como breve apresentação das carreiras que você poderá atuar utilizando seus conhecimentos na área.
+                    </p>
+                    <p>O destaque da plataforma, porém, se encontra na página "Chatbot", onde você poderá conversar com um assistente
+                        virtual que utiliza Inteligência Artificial Generativa para tirar suas dúvidas e ampliar seus conhecimentos sobre
+                        testes de software. E o melhor: você não paga nada por isso!
+                    </p>
+                    <br/>
+                    <h3>Perguntas mais frequentes:</h3>
+                    <h4>Qual o modelo utilizado?</h4>
+                    <p>Atualmente o Smartest utiliza como base o modelo Sabiá-3, desenvolvido pela equipe brasileira Maritaca AI.</p>
+                    
                 </Content>
-            </section>
+            </Container>
         </AppContainer>
     )
 }
--- src/view/components/help/style.ts
+++ src/view/components/help/style.ts
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
+export const Container = styled.section`
+    padding-top: 10px;
+`
+
 export const Title = styled.h1`
     margin-top: 0px;
     text-align: center;
