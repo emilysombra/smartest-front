721b6e1df7ea83891e7af3f43fd6cce88f1a12ac
Emily Sombra <emmevsombra@gmail.com>
2025-01-29 23:58:52 -0300

    fix test configuration

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,10 @@
 export default {
-    preset: 'ts-jest',
-    testEnvironment: 'jest-environment-jsdom',
+    preset: "ts-jest",
+    testEnvironment: "jest-environment-jsdom",
     transform: {
-        "^.+\\.tsx?$": "ts-jest" 
-    // process `*.tsx` files with `ts-jest`
+      "^.+\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.app.json" }]
     },
     moduleNameMapper: {
-        '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__ mocks __/fileMock.ts',
-    },
-}
+      "\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/mocks/fileMock.ts"
+    }
+  };
