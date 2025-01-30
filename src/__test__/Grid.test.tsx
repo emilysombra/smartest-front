3141653f0093cc71fdbf65e0c70901157c66a78a
Emily Sombra <emmevsombra@gmail.com>
2025-01-29 22:03:43 -0300

    tests configuration

---
+++ src/__test__/Grid.test.tsx
@@ -0,0 +1,8 @@
+import '@testing-library/jest-dom'
+import { render } from '@testing-library/react'
+//import Grid from '../view/components/grid'
+
+test("Renders the main page", () => {
+    //render(<Grid data={[]} type={'courses'} />)
+    expect(true).toBeTruthy()
+})
